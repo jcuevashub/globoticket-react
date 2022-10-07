581261440c2ecaa2780335e5760f1b758e6d25d6
Jackson Cuevas <jackson.cuevas@hotmail.com>
2022-10-07 16:53:46 -0400

    Mocking with Mirage JS

--- globoticket-react/src/index.js
+++ globoticket-react/src/index.js
@@ -2,6 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Server } from "miragejs";
+
+new Server({
+  routes() {
+    this.get("http://localhost:3333/events", () => [
+      {
+        id: 1,
+        artist: "Container Enthusiam",
+        name: "Talk Docker to Me Tour",
+        date: "2021-10-01",
+        price: 59.99,
+        imgUrl: "logos/DigitalCowboys_BinaryBovine.png",
+      },
+      {
+        id: 2,
+        artist: "Container Enthusiam",
+        name: "Talk Docker to Me Tour",
+        date: "2021-10-01",
+        price: 59.99,
+        imgUrl: "logos/DigitalCowboys_BinaryBovine.png",
+      },
+    ]);
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
