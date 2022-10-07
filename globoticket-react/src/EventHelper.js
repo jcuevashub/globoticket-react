f4aca14570cd4b687bb4c5cff11dc1cf36e61e07
Jackson Cuevas <jackson.cuevas@hotmail.com>
2022-10-07 11:19:38 -0400

    Adding Pagination

--- globoticket-react/src/EventHelper.js
+++ globoticket-react/src/EventHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export function getEvents() {
-  const response = axios.get(`http://localhost:3333/events`);
+export function getEvents(page) {
+  const response = axios.get(`http://localhost:3333/events?page=${page}`);
   return response;
 }
--- globoticket-react/src/Eventlist.js
+++ globoticket-react/src/Eventlist.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import Eventitem from "./Eventitem";
-import axios from "axios";
 import { getEvents } from "./EventHelper";
 
 export default function Eventlist() {
-  const { isLoading, data } = useQuery("events", () => getEvents());
+  const [page, setPage] = useState(1);
+  const { isLoading, data } = useQuery(["events", page], () => getEvents(page));
 
   if (isLoading) {
     return <div className="container mt-5">Loading ...</div>;
@@ -26,10 +26,32 @@ export default function Eventlist() {
             </tr>
           </thead>
           <tbody>
-            {data.data.map((event) => (
+            {data.data.rows.map((event) => (
               <Eventitem event={event} key={event.id} />
             ))}
           </tbody>
+          <tfoot>
+            <tr>
+              <th colSpan="3" className="text-center">
+                <button
+                  className="btn btn-primary btn-primary-themed btn-md font-upper"
+                  disabled={page === 1}
+                  onClick={() => setPage(Math.max(1, page - 1))}
+                >
+                  Previous
+                </button>
+              </th>
+              <th colSpan="3">
+                <button
+                  className="btn btn-primary btn-primary-themed btn-md font-upper"
+                  disabled={!data.data.hasMore}
+                  onClick={() => setPage(page + 1)}
+                >
+                  Next
+                </button>
+              </th>
+            </tr>
+          </tfoot>
         </table>
       </div>
     </div>
